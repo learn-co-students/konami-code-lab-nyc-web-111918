a99f63c08afdd367c0394795eeadefe9d365b439
MichaelM3 <dagreekarican@aim.com>
2019-01-08 13:58:05 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,23 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+  const body = document.querySelector('body')
+  body.addEventListener('keydown', function(onKeyDownHandler) {
+    const key = onKeyDownHandler.key;
+
+    if (key === codes[index]) {
+      console.log(key)
+      console.log(index)
+      index++;
+
+      if (index === codes.length) {
+        alert("Congrats!");
+
+        index = 0;
+      }
+    } else {
+        index = 0;
+    }
+  })
 }
