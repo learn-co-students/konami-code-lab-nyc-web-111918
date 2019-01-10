5b65b007046c108b229fd892e818a000b4a67fd8
isaac <oseasisaac91@gmail.com>
2019-01-10 09:59:26 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,25 @@ const codes = [
   "b",
   "a"
 ];
+const entered = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('keydown', function(event){
+
+    entered.push(event.key)
+    if(codes.join().includes(entered.join())){
+      console.log(entered)
+      if (codes.join() === entered.join()){
+        alert("Hurray!");
+        entered.splice(0, entered.length)
+      }
+    } else {
+      entered.splice(0, entered.length)
+    }
+  })
+})
 
 function init() {
   // your code here
+
 }
